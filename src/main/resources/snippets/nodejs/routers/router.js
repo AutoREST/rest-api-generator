5eaec196d3a4013d4a2de86d2474adcba055be7b
Lásaro Dumer <ladumer@hotmail.com>
2017-06-04 15:34:14 -0300

    Updated snippets

--- src/main/resources/snippets/nodejs/routers/router.js
+++ src/main/resources/snippets/nodejs/routers/router.js
@@ -3,17 +3,10 @@ var router = express.Router();
 var {{model_name}} = require('../models/{{resource_name}}');
 
 {{head_route}}
-
 {{get_route}}
-
-{{get_route_props}}
-
 {{post_route}}
-
 {{put_route}}
-
 {{patch_route}}
-
 {{delete_route}}
-
+{{props_routes}}
 module.exports = router;
